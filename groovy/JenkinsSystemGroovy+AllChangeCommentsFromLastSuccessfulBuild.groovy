0288cb1d4ba1eb5e113064cfec9a981efb74ea21
Jeongsang Kim <jeongsang.kim@greencar.co.kr>
2022-10-17 09:02:18 +0900

    feat: JenkinsSystemGroovy+AllChangeCommentsFromLastSuccessfulBuild 추가

---
+++ groovy/JenkinsSystemGroovy+AllChangeCommentsFromLastSuccessfulBuild.groovy
@@ -0,0 +1,17 @@
+import jenkins.model.Jenkins
+
+def allChangeCommentsFromLastSuccessfulBuild() {
+    def env = binding.build.environment
+    def job = Jenkins.instance.getItem("${env.JOB_NAME}")
+    def lastSuccessBuildNumber = job.lastSuccessfulBuild.number as int
+    def currentBuildNumber = "${env.BUILD_ID}" as int
+    def range = (lastSuccessBuildNumber + 1)..currentBuildNumber
+
+    return range
+        .collect { job.getBuildByNumber(it) }
+        .collectMany { it.getChangeSets() }
+        .collectMany { it.getLogs() }
+        .collect { it.getComment() }
+}
+
+println allChangeCommentsFromLastSuccessfulBuild().join("")
